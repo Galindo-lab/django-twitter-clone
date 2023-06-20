f79a941654570ed17b22c69a6e93ac9e054d6999
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-06-20 09:18:29 -0700

    ya se pueden hacer twitts

--- social_project/twitter/forms.py
+++ social_project/twitter/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Post
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
@@ -13,3 +13,16 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2'
         ]
+
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={
+        'class':'form-control w-100',
+        'id':'content-box',
+        'rows':'3',
+        'placeholder':'¿que esta pasando?'
+    }))
+
+    class Meta:
+        model = Post
+        fields = ['content']
--- social_project/twitter/views.py
+++ social_project/twitter/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect
 from . models import Profile, Post
-from . forms import UserRegisterForm
+from . forms import UserRegisterForm, PostForm
 
 
 def home(request):
     posts = Post.objects.all()
+
+    if request.method == 'POST':
+        # guardar el post del twitt
+
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('home')
+        
+    else:
+        form = PostForm()
+
     return render(request, 'twitter/newsfeed.html', {
-        'posts': posts
+        'posts': posts,
+        'form': form
     })
 
 
 def register(request):
 
     if request.method == 'POST':
+
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
