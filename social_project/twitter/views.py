64f01e85a0883ed2dc33f239f03f8cc2bab43e53
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-06-19 22:35:53 -0700

    registro terminado

--- social_project/twitter/views.py
+++ social_project/twitter/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Profile, Post
+from . forms import UserRegisterForm
+
 
 def home(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'twitter/newsfeed.html', context)
+    return render(request, 'twitter/newsfeed.html', {
+        'posts': posts
+    })
+
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        return render(request, 'twitter/register.html', {
+            'form': UserRegisterForm()
+        })
