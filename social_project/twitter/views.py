8bd53af90c0fcbae98725214060ed4b282f21441
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-06-21 16:18:59 -0700

    Modificacion de perfil agregado

--- social_project/twitter/views.py
+++ social_project/twitter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . models import Profile, Post
-from . forms import UserRegisterForm, PostForm
+from . forms import UserRegisterForm, PostForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -45,3 +46,40 @@ def delete(request, post_id):
     post = Post.objects.get(id=post_id)
     post.delete()
     return redirect('home')
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    posts = user.posts.all()
+
+    return render(request, 'twitter/profile.html', {
+        'user': user,
+        'posts': posts
+    })
+
+
+def editar(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(
+            request.POST,
+            instance=request.user
+        )
+
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('home')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm()
+
+    return render(request, 'twitter/editar.html', {
+        'u_form': u_form,
+        'p_form': p_form
+    })
